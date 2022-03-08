17562db0fe1fd132bcb234a2841f9d8033fae399
flaviobartolo <flavio.bartolo2@gmail.com>
2022-03-08 01:05:56 +0000

    Section 6 finished: Building a Blog

---
+++ blog/data/posts.py
@@ -0,0 +1,32 @@
+posts_data = [
+  {
+    'author':'anthony',
+    'slug': 'lorem-ipsum',
+    'title':'Lorem Ipsum',
+    'text':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium lectus quam id leo in vitae turpis massa sed. Dolor magna eget est lorem. Tortor dignissim convallis aenean et tortor at risus viverra. Mattis aliquam faucibus purus in massa tempor nec feugiat nisl. Fames ac turpis egestas sed tempus urna. Condimentum mattis pellentesque id nibh tortor id. Pulvinar etiam non quam lacus suspendisse faucibus.',
+  },
+  {
+    'author':'keith',
+    'slug': 'feugiat-pretium',
+    'title':'Feugiat Pretium',
+    'text':'Condimentum mattis pellentesque id nibh tortor id. Pulvinar etiam non quam lacus suspendisse faucibus. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Rhoncus dolor purus non enim praesent elementum facilisis.',
+  },
+  {
+    'author':'mary',
+    'slug': 'habitasse-platea',
+    'title':'Habitasse platea',
+    'text':'Tortor at risus viverra adipiscing at in tellus integer. Et netus et malesuada fames ac turpis egestas. Aliquet eget sit amet tellus cras adipiscing enim eu.',
+  },
+  {
+    'author':'stefanny',
+    'slug': 'facilisi-nullam',
+    'title':'Facilisi nullam',
+    'text':'Ornare quam viverra orci sagittis eu volutpat odio facilisis.',
+  },
+  {
+    'author':'john',
+    'slug': 'arcu-dictum',
+    'title':'Arcu dictum',
+    'text':'Quam id leo in vitae turpis massa sed elementum.',
+  },
+]
--- my_blog/settings.py
+++ my_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'my_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
